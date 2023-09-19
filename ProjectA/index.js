8f56735363415767483cef3e910388b253800bae
devfabien <ish001fabi@gmail.com>
2023-09-19 13:03:04 +0200

    Project A rendering a function component

--- ProjectA/index.js
+++ ProjectA/index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<ul><li>data one</li><li>data two</li></ul>,document.getElementById('root'))
+function Maincontent(){
+    return(
+        <div>
+        <h1>will it work</h1>
+        <p>let's try and see</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<div>
+    <Maincontent/>
+</div>,document.getElementById('root'))
