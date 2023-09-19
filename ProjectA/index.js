8d444acd3fa4c755c715321c84710b0d2b1241ed
devfabien <ish001fabi@gmail.com>
2023-09-19 14:56:03 +0200

    Project A rendering a jsx element

--- ProjectA/index.js
+++ ProjectA/index.js
@@ -1,12 +1,20 @@
-function Maincontent(){
-    return(
-        <div>
-        <h1>will it work</h1>
-        <p>let's try and see</p>
-        </div>
-    )
-}
+// function Maincontent(){
+//     return(
+//         <div>
+//         <h1>will it work</h1>
+//         <p>let's try and see</p>
+//         </div>
+//     )
+// }
+const navbar=(
+    <nav>
+    <h1>Devfabien</h1>
+    <ul>
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contact</li>
+    </ul>
+    </nav>
+)
 
-ReactDOM.render(<div>
-    <Maincontent/>
-</div>,document.getElementById('root'))
+ReactDOM.render(navbar,document.getElementById('root'))
