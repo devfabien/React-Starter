60e881a72257968f707520ce689d5f22f1e0f54b
devfabien <ish001fabi@gmail.com>
2023-09-20 19:01:36 +0200

    Airbnb clone adding the card component

---
+++ Airbnb/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import image1 from '../assets/image1.png';
+import star from '../assets/Star.png';
+export default function Card() {
+    return (
+        <div className="card">
+            <img src={image1} className="card--image" />
+            <div className="card--stats">
+                <img src={star} className="card--star" />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
+}
