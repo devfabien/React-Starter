1a537e581cec5129cc8bcbc85487ac3634ca224a
devfabien <ish001fabi@gmail.com>
2023-09-20 21:17:32 +0200

    Airbnb clone adding the props for displaying cards

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -1,6 +1,8 @@
 
 import Navbar from './components/Navbar'
 import './App.css'
+import image2 from './assets/image2.png';
+import image1 from './assets/image1.png';
 import Hero from './components/Hero'
 import Card from './components/Card'
 
@@ -11,7 +13,22 @@ function App() {
     <>
      <Navbar/>
      <Hero/>
-     <Card/>
+     <div style={{display:'flex'}}>
+     <Card
+     image={image1}
+     ratio='5.0'
+     num='6'
+     info='Life lessons with Katie Zaferes '
+     money='136'
+     />
+     <Card
+     image={image2}
+     ratio='5.0'
+     num='30'
+     info='Learn wedding photography'
+     money='125'
+     />
+     </div>
     </>
   )
 }
--- Airbnb/src/components/Card.jsx
+++ Airbnb/src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import image1 from '../assets/image1.png';
 import star from '../assets/Star.png';
-export default function Card() {
+export default function Card({image,ratio,num,info,money}) {
     return (
         <div className="card">
-            <img src={image1} className="card--image" />
+            <img src={image} className="card--image" />
             <div className="card--stats">
                 <img src={star} className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
+                <span>{ratio}</span>
+                <span className="gray">({num}) • </span>
                 <span className="gray">USA</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{info}</p>
+            <p><span className="bold">From ${money}</span> / person</p>
         </div>
     )
 }
