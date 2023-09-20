47ddf7e80317108676d0612babf45453a4e96328
devfabien <ish001fabi@gmail.com>
2023-09-20 11:41:00 +0200

    ExerciseA Adding the header component

---
+++ firstExercise/index.js
@@ -0,0 +1,20 @@
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./React-icon.png" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+
+
+
+
+
