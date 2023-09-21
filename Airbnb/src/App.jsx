f40fe569ff42dc73303e7646bca5bb3df1ae4c39
devfabien <ish001fabi@gmail.com>
2023-09-21 15:55:40 +0200

    Airbnb clone
    adding rendering the page

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -1,34 +1,21 @@
 
 import Navbar from './components/Navbar'
 import './App.css'
-import image2 from './assets/image2.png';
-import image1 from './assets/image1.png';
 import Hero from './components/Hero'
 import Card from './components/Card'
+import CardData from './components/CardData'
 
 function App() {
  
-
+const CardElement=CardData.map((card,id)=><Card key={id} image={card.image} ratio={card.ratio} num={card.num} info={card.info} money={card.money} openSpot={card.openSpot}/>)
   return (
     <>
      <Navbar/>
      <Hero/>
-     <div style={{display:'flex'}}>
-     <Card
-     image={image1}
-     ratio='5.0'
-     num='6'
-     info='Life lessons with Katie Zaferes '
-     money='136'
-     />
-     <Card
-     image={image2}
-     ratio='5.0'
-     num='30'
-     info='Learn wedding photography'
-     money='125'
-     />
-     </div>
+     <section className="cards-list">
+                {CardElement}
+            </section>
+    
     </>
   )
 }
