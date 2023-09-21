fb5a98de0f91c8dce50185ccbe86e549eea868aa
devfabien <ish001fabi@gmail.com>
2023-09-21 15:53:32 +0200

    Airbnb clone
    adding the data file

---
+++ Airbnb/src/components/CardData.jsx
@@ -0,0 +1,50 @@
+export default [
+    {
+        image:"image1.png",
+        num:5.0,
+        ratio:6,
+        info:"Life lessons with Katie Zaferes",
+        money:136,
+        openSpot:3
+    },
+    {
+        image:"image2.png",
+        num:5.0,
+        ratio:"30",
+        info:"Learn wedding photography",
+        money:125,
+        openSpot:0
+    },
+    {
+        image:"image3.png",
+        num:4.8,
+        ratio:2,
+        info:"Group Mountain Biking",
+        money:50,
+        openSpot:2
+        },
+        {
+            image:"image1.png",
+            num:5.0,
+            ratio:6,
+            info:"Life lessons with Katie Zaferes",
+            money:136,
+            openSpot:0
+        },
+        {
+            image:"image2.png",
+            num:5.0,
+            ratio:"30",
+            info:"Learn wedding photography",
+            money:125,
+            openSpot:0
+        },
+        {
+            image:"image3.png",
+            num:4.8,
+            ratio:2,
+            info:"Group Mountain Biking",
+            money:50,
+            openSpot:0
+            },
+]
